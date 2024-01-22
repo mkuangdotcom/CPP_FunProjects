2a2d9678e7b230dcd99693881d274cc49cb5783e
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-01-22 17:37:36 +0800

    Update Sales_analysis.cpp

--- Sales_analysis.cpp
+++ Sales_analysis.cpp
@@ -65,10 +65,14 @@ int main () {
 
     outFile << "Profitable stores: " << endl;
     outFile << "-----------------" << endl;
-    for (int r =0; r < STORE; r++) {
+    for (int r = 0; r < STORE; r++) {
         if (storeTotalSales(salesData, r) > 600000.00)
             outFile << store[r] << endl;
     }
+
+    return 0;
+}
+
 
     return 0;
 }
